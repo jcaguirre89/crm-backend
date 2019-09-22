633dfd31f47ad19bc60f96396c799cbc72d3a3aa
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-09-22 10:48:49 -0700

    formatted views

--- crm/views.py
+++ crm/views.py
@@ -1,47 +1,64 @@
 from rest_framework import viewsets
-from .serializers import (CompanySerializer, CompanyNoteSerializer,
-                          ContactSerializer, ContactNoteSerializer,
-                          DealSerializer, DealNoteSerializer)
+from .serializers import (
+    CompanySerializer,
+    CompanyNoteSerializer,
+    ContactSerializer,
+    ContactNoteSerializer,
+    DealSerializer,
+    DealNoteSerializer,
+)
 from .models import Company, Deal, Contact, CompanyNote, DealNote, ContactNote
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     """ Company CRUD """
+
     serializer_class = CompanySerializer
-    model = Company
+    queryset = Company.objects.all()
 
 
 class CompanyNoteViewSet(viewsets.ModelViewSet):
     """ CompanyNote CRUD """
+
     serializer_class = CompanyNoteSerializer
-    model = CompanyNote
+
+    def get_queryset(self):
+        return CompanyNote.objects.filter(company=self.kwargs["company_pk"])
 
 
 class ContactViewSet(viewsets.ModelViewSet):
     """ Contact CRUD """
+
     serializer_class = ContactSerializer
-    model = Contact
+
+    def get_queryset(self):
+        return Contact.objects.filter(company=self.kwargs["company_pk"])
 
 
 class ContactNoteViewSet(viewsets.ModelViewSet):
     """ ContactNote CRUD """
+
     serializer_class = ContactNoteSerializer
-    model = ContactNote
+
+    def get_queryset(self):
+        return ContactNote.objects.filter(contact=self.kwargs["contact_pk"])
 
 
 class DealViewSet(viewsets.ModelViewSet):
     """ Deal CRUD """
+
     serializer_class = DealSerializer
     model = Deal
+
+    def get_queryset(self):
+        return Deal.objects.filter(company=self.kwargs["company_pk"])
 
 
 class DealNoteViewSet(viewsets.ModelViewSet):
     """ DealNote CRUD """
+
     serializer_class = DealNoteSerializer
     model = DealNote
 
-
-class CompanyViewSet(viewsets.ModelViewSet):
-    """ Company CRUD """
-    serializer_class = CompanySerializer
-    model = Company
+    def get_queryset(self):
+        return DealNote.objects.filter(deal=self.kwargs["deal_pk"])
